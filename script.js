c7adca9470d7114219ba681214023f303c10eef5
Eshan Aditya <83403698+eshangonemad@users.noreply.github.com>
2023-01-20 15:20:36 +0000

    The title was added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function createPDF(){
 let text = document.getElementById("textarea").value;
+let title = document.getElementById("title").value
 var doc = new jsPDF();
 doc.text(20, 20, text);
-doc.save('document.pdf');
+doc.save(title+'.pdf');
 }
