677d8c4348bc7a980a26108943e3388f694d7a97
Eshan Aditya <83403698+eshangonemad@users.noreply.github.com>
2023-01-27 12:29:07 +0000

    added availablilty for 2 images. atleast i tried rite?

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-
+function mavi(){
+  
+document.getElementById("2").style.display = "none";
+}
+var nomr= 1
+console.log(nomr)
 var img={};
+var img2={}
   function encodeImageFileAsURL(element) {
-        let file = element.files[0];
+   
+            let file = element.files[0];
         let reader = new FileReader();
         reader.onloadend = function() {
         img = reader.result
           console.log(img)
+          nomr++
+          console.log(nomr)
+         if (nomr==2){
+           
+document.getElementById("2").style.display = "block";
+         }
         }
         reader.readAsDataURL(file);
 
-      }
+  }
+  function q(element) {
+   
+            let file = element.files[0];
+        let reader = new FileReader();
+        reader.onloadend = function() {
+        img2 = reader.result
+          console.log(img)
+          nomr++
+          console.log(nomr)
+         if (nomr==3){
+           
+document.getElementById("2").style.display = "block";
+         }
+        }
+        reader.readAsDataURL(file);
+  }
+
 
     function createPDF() {
   let text = document.getElementById("textarea").value;
@@ -17,6 +47,7 @@ var img={};
   let title = document.getElementById("title").value
   var doc = new jsPDF();
   doc.text(20, 20, text);
-  doc.addImage(img, 'png',20,20)
+  doc.addImage(img, 'png',20,20)  
+  doc.addImage(img2, 'png',20,20)
   doc.save(title + '.pdf');
 };
