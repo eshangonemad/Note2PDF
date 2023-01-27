69e7d7d3d4fcbd31fd655cf39ddf3457ac6ad685
Eshan Aditya <83403698+eshangonemad@users.noreply.github.com>
2023-01-27 09:55:28 +0000

    a loot lama

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function createPDF(){
-let text = document.getElementById("textarea").value;
-let title = document.getElementById("title").value
-var doc = new jsPDF();
-doc.text(20, 20, text);
-doc.save(title+'.pdf');
-}
+
+var img={};
+  function encodeImageFileAsURL(element) {
+        let file = element.files[0];
+        let reader = new FileReader();
+        reader.onloadend = function() {
+        img = reader.result
+          console.log(img)
+        }
+        reader.readAsDataURL(file);
+
+      }
+
+    function createPDF() {
+  let text = document.getElementById("textarea").value;
+
+  let title = document.getElementById("title").value
+  var doc = new jsPDF();
+  doc.text(20, 20, text);
+  doc.addImage(img, 'png',20,20)
+  doc.save(title + '.pdf');
+};
